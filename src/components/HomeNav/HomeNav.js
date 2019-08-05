62c40baf9c8e98e0812dbde8e1de633c13a216c4
mmacdonald1 <melmacd94@gmail.com>
2019-08-05 17:35:57 -0400

    update navbar

--- src/components/HomeNav/HomeNav.js
+++ src/components/HomeNav/HomeNav.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {withRouter} from "react-router-dom"
 import {
   Collapse,
   Navbar,
@@ -16,34 +17,17 @@ class HomeNav extends Component{
   render(){
     return(
       <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/#">reactstrap</NavbarBrand>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Luce</NavbarBrand>
           <NavbarToggler />
           <Collapse navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                <NavLink href="/cart">Cart</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink href="/contact">Contact</NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
